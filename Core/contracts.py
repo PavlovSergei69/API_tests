9768bdf2e6616e27551487f15ea828a810b6461f
PavlovSergei69 <serezhaunknown@gmail.com>
2025-05-13 11:45:46 +0300

    Урок №1. Докорректировал код от 13.05.2025. Изменил название ветки

--- Core/contracts.py
+++ Core/contracts.py
@@ -1,27 +1,49 @@
 LIST_RESOURCE_SCHEME = {
-    'type':'object',
-    'properties':{
-        'id':{'type':'number'},
-        'name':{'type':'string'},
-        'year':{'type':'number'},
-        'color':{'type':'string'},
-        'pantone_value':{'type':'string'}
+    "type":"object",
+    "properties": {
+        "id": {
+            "type":"number"
+        },
+        "name": {
+            "type":"string"
+        },
+        "year": {
+            "type":"number"
+        },
+        "color": {
+            "type":"string"
+        },
+        "pantone_value": {
+            "type":"string"
+        }
     },
-    'requiered':['id', 'name', 'year', 'color','pantone_value']
+    "required":["id", "name", "year", "color","pantone_value"],
+    "additionalProperties": False
 }
 
-#договоренность между двуями сторонами, что должно приходить в ответе
+#договоренность между двумя сторонами, что должно приходить в ответе
 USER_DATA_SHEME = {
-    'type':'object',
+    "type":"object",
     #характеристики
-    'properties':{
-        'id':{'type':'number'},
-        'email':{'type':'string'},
-        'first_name':{'type':'string'},
-        'last_name':{'type':'string'},
-        'avatar':{'type':'string'}
+    "properties": {
+        "id": {
+            "type":"number"
+        },
+        "email": {
+            "type":"string"
+        },
+        "first_name": {
+            "type":"string"
+        },
+        "last_name": {
+            "type":"string"
+        },
+        "avatar": {
+            "type":"string"
+        }
     },
     #Обязательные параметры
-    'requierd':['id', 'email','first_name','last_name', 'avatar']
+    "required":["id", "email","first_name","last_name", "avatar"],
+    "additionalProperties": False
 }
 
--- Tests/test_get_user_data.py
+++ Tests/test_get_user_data.py
@@ -3,33 +3,33 @@ import httpx
 from jsonschema import validate
 from Core.contracts import USER_DATA_SHEME
 
-BASE_URL = 'https://reqres.in/'
-LIST_USERS = 'api/users?page=2'
-SINGLE_USER = 'api/users/2'
-USER_NOT_FOUND = 'api/users/23'
+BASE_URL = "https://reqres.in/"
+LIST_USERS = "api/users?page=2"
+SINGLE_USER = "api/users/2"
+USER_NOT_FOUND = "api/users/99"
 
-EMAIL_ENDS = '@reqres.in'
-AVATAR_ENDS = '-image.jpg'
+EMAIL_ENDS = "@reqres.in"
+AVATAR_ENDS = "-image.jpg"
 
 def test_list_users():
-    responce = httpx.get(BASE_URL + LIST_USERS)
+    response = httpx.get(BASE_URL + LIST_USERS)
     #assert инструмент для проверки того, что возвращается. Два "==" необходимо для сравнения, одно "=" для присваивания данных переменной
-    assert responce.status_code == 200
-    data = responce.json()['data']
-    #цикл проверяет отдельно каждый элемент date последовательно
+    assert response.status_code == 200
+    data = response.json()["data"]
+    #цикл проверяет отдельно каждый элемент date последовательно со схемой USER_DATA_SCHEME
     for item in data:
         validate(item, USER_DATA_SHEME)      #Сначала проверяем "что проверяем", а потом "с каким контрактом" сравниваем
         #endswath = "оканчивается на"
-        assert item['email'].endswith(EMAIL_ENDS)
-        assert item['avatar'].endswith(str(item['id']) + AVATAR_ENDS)
+        assert item["email"].endswith(EMAIL_ENDS)
+        assert item["avatar"].endswith(str(item["id"]) + AVATAR_ENDS)
 
-def test_single_users():
-    responce = httpx.get(BASE_URL + SINGLE_USER)
-    assert responce.status_code == 200
-    date = responce.json()['data']
-    assert date['email'].endswith(EMAIL_ENDS)
-    assert date['avatar'].endswith(str(date['id']) + AVATAR_ENDS)
+def test_single_user():
+    response = httpx.get(BASE_URL + SINGLE_USER)
+    assert response.status_code == 200
+    date = response.json()["data"]
+    assert date["email"].endswith(EMAIL_ENDS)
+    assert date["avatar"].endswith(str(date["id"]) + AVATAR_ENDS)
 
 def test_user_not_found():
-    responce = httpx.get(BASE_URL + USER_NOT_FOUND)
-    assert responce.status_code == 404
+    response = httpx.get(BASE_URL + USER_NOT_FOUND)
+    assert response.status_code == 404
